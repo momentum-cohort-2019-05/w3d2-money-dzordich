306105b6e883e8d8d76758fafd7d364e3b99dffc
dzordich <dzordich@gmail.com>
2019-06-11 15:14:56 -0400

    normal mode complete with all tests passed. will be working on advanced mode tonight

--- money.py
+++ money.py
@@ -15,13 +15,14 @@ class Currency:
         - symbol - optional symbol used to designate currency
         - digits -- number of significant digits used
         """
-        pass
+        self.name = name
+        self.code = code
+        self.symbol = symbol
+        self.digits = digits
+
 
     def __str__(self):
-        """
-        Should return the currency code, or code with symbol in parentheses.
-        """
-        pass
+        return self.code()
 
     def __eq__(self, other):
         """
@@ -43,7 +44,8 @@ class Money:
         - amount -- quantity of currency
         - currency -- type of currency
         """
-        pass
+        self.amount = amount
+        self.currency = currency
 
     def __str__(self):
         """
@@ -70,23 +72,32 @@ class Money:
         Add two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-        pass
+        if self.currency != other.currency:
+            raise DifferentCurrencyError
+        self.amount = self.amount + other.amount
+        return self
+        
 
     def sub(self, other):
         """
         Subtract two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-        pass
+        if self.currency != other.currency:
+            raise DifferentCurrencyError
+        self.amount = self.amount - other.amount
+        return self
 
     def mul(self, multiplier):
         """
         Multiply a money object by a number to get a new money object.
         """
-        pass
+        self.amount = self.amount * multiplier
+        return self
 
     def div(self, divisor):
         """
         Divide a money object by a number to get a new money object.
         """
-        pass
+        self.amount = self.amount / divisor
+        return self
